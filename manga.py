b09076005a54b7e3aca7a9bea3aaf3783e861bfb
Gaysha1337 <33755321+Gaysha1337@users.noreply.github.com>
2018-12-25 18:32:26 +0100

    Update manga.py
    Added manga summary + better chapter folder names

--- manga.py
+++ manga.py
@@ -12,7 +12,7 @@ print("COPY THIS BIT FROM THE LINK ABOVE: read_tokyo_ghoul_manga_online_free4 AN
 print("\n")
 
 print("PLEASE NOTE DEPENDING ON THE AMOUNT OF CHAPTERS IN YOUR SPECFIED MANGA, THIS PROCESS MIGHT TAKE A WHILE")
-print("PLEASE NOTE THAT YOU MAY ONLY RENAME THE FOLDERS ONCE YOUR MANGA HAS BEEN DOWNLOADED, ELSE THE SCRIPT WILL CRASH")
+print("PLEASE NOTE THAT YOU MAY ONLY RENAME THE FOLDERS ONCE YOUR MANGA HAS BEEN DOWNLOADED, OR ELSE THE SCRIPT WILL CRASH")
 print("\n")
 
 manga_input = input("Type or paste a manga name ").strip().lower()
@@ -35,6 +35,15 @@ os.chdir(current_manga_dir)
 
 def write_meta_data_file():
     meta_data_li_tags = soup.find("ul",attrs={"class":"manga-info-text"}).findChildren("li")
+    summary_div = soup.find("div",attrs={"id":"noidungm"})
+    split_summary = summary_div.text.strip().split()[1::]
+    formatted_summary = "\n"    
+    for word in split_summary:
+        if "." in word or "," in word or word == split_summary[1]:
+            word+="\n"
+        formatted_summary += word + " "
+    print(formatted_summary)
+        
     meta_data_list = [tag.text.strip() for tag in meta_data_li_tags]
     for datum in meta_data_list:
         meta_data_list.pop(4)
@@ -42,6 +51,7 @@ def write_meta_data_file():
     meta_data_list[1] = meta_data_list[1].replace("\n","")
     with open("info.txt","w",encoding='utf-8') as f:
         f.write("\n".join(meta_data_list))
+        f.write(formatted_summary)
         
 url = "https://manganelo.com/manga/{}".format(manga_input.replace(" ","_")) # url = "https://manganelo.com/manga/read_deadman_wonderland"
 chapter_list_html = requests.get(url).content
@@ -52,6 +62,7 @@ chapter_divs = soup.find_all("div",attrs={"class":"row"})
 
 # CHAPTER RELATED
 chapter_links = [a.get("href") for div in chapter_divs for span in div.findChildren("span") for a in span.find_all("a")][::-1]
+chapter_names = iter([a.get("title") for div in chapter_divs for span in div.findChildren("span") for a in span.find_all("a")][::-1])
 number_of_chapters = len(chapter_links)
 
 write_meta_data_file()
@@ -63,12 +74,14 @@ for link in chapter_links:
     img_holder_div = current_chapter_soup.find("div",attrs={"id":"vungdoc"})
     img_links = [link.get("src") for link in img_holder_div.find_all("img")]
     print("------------------------- Chapter Link Appended: {} ----------------------------".format(current_chapter_number))
-    chapter_info = {"chapter_number":current_chapter_number,"img_links":img_links}
+    chapter_info = {"chapter_number":current_chapter_number,"chapter_name":next(chapter_names),"img_links":img_links}
     img_info.append(chapter_info)
-
+    
 for img_dict in img_info:
     img_chapter = img_dict.get("chapter_number")
-    current_chapter_dir = os.path.join(current_manga_dir,str(img_chapter))
+    chapter_name = img_dict.get("chapter_name")
+    current_chapter_dir = os.path.join(current_manga_dir,str(chapter_name).replace(":"," - "))
+
     for link in img_dict.get("img_links"):
         current_page_number = img_dict.get("img_links").index(link) + 1
         
@@ -83,17 +96,5 @@ for img_dict in img_info:
             f.write(img_request.content)
     print("------------- Dowloaded Chapter {}; there are {} pages in this chapter ----------".format(img_chapter,len(img_dict.get("img_links"))))
 
+
 print("All chapters downloaded")
-
-
-
-
-        
-        
-        
-        
-    
-
-            
-
-
